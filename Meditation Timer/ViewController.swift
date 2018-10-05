9335b3c2030c976967cbd16640c897cf4dc81fe7
Nicholas Cooke <ncooke3@gatech.edu>
2018-10-05 15:44:40 -0400

    Initial Commit

---
+++ Meditation Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meditation Timer
+//
+//  Created by Nicholas Cooke on 10/5/18.
+//  Copyright © 2018 Nicholas Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
